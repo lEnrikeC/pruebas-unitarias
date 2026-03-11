94886a71b34c89e57b5c45df90d1e8b99e2f17ca
lEnrikeC <luiscortes02@uv.mx>
2026-03-11 13:21:50 -0600

    primer cambio pruebas (#1)

--- tests/Calculadora.Tests/OperacionesMatematicasTests.cs
+++ tests/Calculadora.Tests/OperacionesMatematicasTests.cs
@@ -3,6 +3,44 @@ namespace Calculadora.Tests;
 
 public class OperacionesMatematicasTests
 {
-    
 
+    private readonly OperacionesMatematicas _operacionesMatematicas = new OperacionesMatematicas();
+
+    [Fact]
+    public void Sumar_DosPositivos_RetornaSuma()
+    {
+        // Arrange
+        int a = 3, b = 6;   
+
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(9, resultado);
+    }
+
+    [Fact]
+    public void Sumar_DosNegativoMasPositivo_RetornaValorCorrecto()
+    {
+        // Arrange
+        int a = -10, b = 10;
+
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(0, resultado);
+    }
+
+    [Fact]
+    public void Sumar_CeroMasCero_RetornaCero(){
+
+        // Arrange
+        int a = 0, b = 0;
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(0, resultado);
+    }
 }
