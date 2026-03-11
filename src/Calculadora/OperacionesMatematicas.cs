9a7bd5c54957ab62985212a118fd6006236d7fd4
Enrique <luiscortes02@uv.mx>
2026-03-11 13:23:00 -0600

    primer cambio pruebas

---
+++ src/Calculadora/OperacionesMatematicas.cs
@@ -0,0 +1,16 @@
+namespace Calculadora;
+
+public class OperacionesMatematicas
+{
+    public int Sumar(int a, int b) => a + b;
+    public int Restar(int a, int b) => a - b;
+    public int Multiplicar(int a, int b) => a * b;
+    public double Dividir(int a, int b) 
+    {
+        if (b == 0)
+            throw new DivideByZeroException("No se puede dividir por cero");
+        return a / b;
+    }
+
+    public bool EsPar(int numero) => numero % 2 == 0;
+}
